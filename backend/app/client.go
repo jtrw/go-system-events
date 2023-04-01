8b2a020c8d70b553def5824fcdf73ac4dc1d72b1
Nil Borodulia <nil.b@oxtech.org>
2023-04-01 14:38:00 +0300

    fix struct

--- backend/app/client.go
+++ backend/app/client.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+    "log"
+    "time"
+    "encoding/json"
+    "github.com/gorilla/websocket"
+)
 
 // ClientList is a map used to help manage a map of clients
 type ClientList map[*Client]bool
@@ -14,7 +19,18 @@ type Client struct {
 	manager *Manager
 
 	egress chan []byte
+
+	chatroom string
 }
+
+var (
+	// pongWait is how long we will await a pong response from client
+	pongWait = 10 * time.Second
+	// pingInterval has to be less than pongWait, We cant multiply by 0.9 to get 90% of time
+	// Because that can make decimals, so instead *9 / 10 to get 90%
+	// The reason why it has to be less than PingRequency is becuase otherwise it will send a new Ping before getting response
+	pingInterval = (pongWait * 9) / 10
+)
 
 // NewClient is used to initialize a new Client with all required values initialized
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
@@ -45,6 +61,18 @@ func (c *Client) readMessages() {
 			}
 			break // Break the loop to close conn & Cleanup
 		}
+
+		// Marshal incoming data into a Event struct
+        var request Event
+        if err := json.Unmarshal(payload, &request); err != nil {
+            log.Printf("error marshalling message: %v", err)
+            break // Breaking the connection here might be harsh xD
+        }
+        // Route the Event
+        if err := c.manager.routeEvent(request, c); err != nil {
+            log.Println("Error handeling Message: ", err)
+        }
+
 		log.Println("MessageType: ", messageType)
 		log.Println("Payload: ", string(payload))
 
@@ -56,8 +84,16 @@ func (c *Client) readMessages() {
 	}
 }
 
+// pongHandler is used to handle PongMessages for the Client
+func (c *Client) pongHandler(pongMsg string) error {
+	// Current time + Pong Wait time
+	log.Println("pong")
+	return c.connection.SetReadDeadline(time.Now().Add(pongWait))
+}
+
 // writeMessages is a process that listens for new messages to output to the Client
 func (c *Client) writeMessages() {
+	ticker := time.NewTicker(pingInterval)
 	defer func() {
 		// Graceful close if this triggers a closing
 		c.manager.removeClient(c)
@@ -66,22 +102,35 @@ func (c *Client) writeMessages() {
 	for {
 		select {
 		case message, ok := <-c.egress:
-			// Ok will be false Incase the egress channel is closed
-			if !ok {
-				// Manager has closed this connection channel, so communicate that to frontend
-				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
-					// Log that the connection is closed and the reason
-					log.Println("connection closed: ", err)
-				}
-				// Return to close the goroutine
-				return
-			}
-			// Write a Regular text message to the connection
-			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
-				log.Println(err)
-			}
-			log.Println("sent message")
-		}
+            // Ok will be false Incase the egress channel is closed
+            if !ok {
+                // Manager has closed this connection channel, so communicate that to frontend
+                if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+                    // Log that the connection is closed and the reason
+                    log.Println("connection closed: ", err)
+                }
+                // Return to close the goroutine
+                return
+            }
+
+            data, err := json.Marshal(message)
+            if err != nil {
+                log.Println(err)
+                return // closes the connection, should we really
+            }
+            // Write a Regular text message to the connection
+            if err := c.connection.WriteMessage(websocket.TextMessage, data); err != nil {
+                log.Println(err)
+            }
+            log.Println("sent message")
+        case <-ticker.C:
+            log.Println("ping")
+            // Send the Ping
+            if err := c.connection.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+                log.Println("writemsg: ", err)
+                return // return to break this goroutine triggeing cleanup
+            }
+        }
 
 	}
 }
--- backend/app/manager.go
+++ backend/app/manager.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 	"sync"
-
+    "errors"
 	"github.com/gorilla/websocket"
 )
 
@@ -18,6 +18,10 @@ var (
 	}
 )
 
+var (
+	ErrEventNotSupported = errors.New("this event type is not supported")
+)
+
 // Manager is used to hold references to all Clients Registered, and Broadcasting etc
 type Manager struct {
 	clients ClientList
@@ -25,12 +29,37 @@ type Manager struct {
 	// Using a syncMutex here to be able to lcok state before editing clients
 	// Could also use Channels to block
 	sync.RWMutex
+	// handlers are functions that are used to handle Events
+	handlers map[string]EventHandler
 }
 
 // NewManager is used to initalize all the values inside the manager
 func NewManager() *Manager {
-	return &Manager{
-		clients: make(ClientList),
+	m := &Manager{
+        clients:  make(ClientList),
+        handlers: make(map[string]EventHandler),
+    }
+    m.setupEventHandlers()
+    return m
+}
+
+// setupEventHandlers configures and adds all handlers
+func (m *Manager) setupEventHandlers() {
+    m.handlers[EventSendMessage] = SendMessageHandler
+	m.handlers[EventChangeRoom] = ChatRoomHandler
+}
+
+// routeEvent is used to make sure the correct event goes into the correct handler
+func (m *Manager) routeEvent(event Event, c *Client) error {
+	// Check if Handler is present in Map
+	if handler, ok := m.handlers[event.Type]; ok {
+		// Execute the handler and return any err
+		if err := handler(event, c); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		return ErrEventNotSupported
 	}
 }
 
