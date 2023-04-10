1554b30151d1471256dbd690a9131d5c808791d1
Nil Borodulia <nil.b@oxtech.org>
2023-04-10 10:23:55 +0300

    add server

--- backend/app/main.go
+++ backend/app/main.go
@@ -4,7 +4,13 @@ import (
     "fmt"
     "log"
     "net/http"
+    "github.com/jessevdk/go-flags"
 )
+
+type Options struct {
+   Host string `short:"h" long:"host" default:"127.0.0.1" description:"Host web server"`
+   Port string `short:"p" long:"port" default:"8080" description:"Port web server"`
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Home Page")
@@ -19,7 +25,31 @@ func setupRoutes() {
 }
 
 func main() {
-    fmt.Println("Hello World")
-    setupRoutes()
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    var opts Options
+    parser := flags.NewParser(&opts, flags.Default)
+    _, err := parser.Parse()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+
+   // setupRoutes()
+   // log.Fatal(http.ListenAndServe(":8080", nil))
+
+
+     srv := Server {
+        Host: opts.Host,
+        Port: opts.Port,
+        PinSize: 1,
+        WebRoot: "/",
+        Version: "1.0",
+    }
+
+    if err := srv.Run(); err != nil {
+        log.Printf("[ERROR] failed, %+v", err)
+    }
+
+    log.Printf("[INFO] Activate rest server")
+    log.Printf("[INFO] Host: %s", opts.Host)
+    log.Printf("[INFO] Port: %s", opts.Port)
 }
---
+++ backend/app/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+ "fmt"
+ "time"
+ "net/http"
+ lgr "github.com/go-pkgz/lgr"
+ "github.com/pkg/errors"
+
+ //"github.com/didip/tollbooth/v6"
+ //"github.com/didip/tollbooth_chi"
+ "github.com/go-chi/chi/v5"
+ "github.com/go-chi/chi/v5/middleware"
+)
+
+type Server struct {
+    Host           string
+    Port           string
+	PinSize        int
+	MaxPinAttempts int
+	WebRoot        string
+	Version        string
+}
+
+func (s Server) Run() error {
+	if err := http.ListenAndServe(s.Host+":"+s.Port, s.routes()); err != http.ErrServerClosed {
+		return errors.Wrap(err, "server failed")
+	}
+	return nil
+}
+
+func (s Server) routes() chi.Router {
+	router := chi.NewRouter()
+
+    router.Use(middleware.Logger)
+	router.Use(middleware.Throttle(1000), middleware.Timeout(60*time.Second))
+	//router.Use(tollbooth_chi.LimitHandler(tollbooth.NewLimiter(10, nil)))
+
+    manager := NewManager()
+
+	router.Route("/", func(r chi.Router) {
+	    r.Get("/", s.homePage)
+	    r.Post("/ws", manager.serveWS)
+	})
+
+    lgr.Printf("[INFO] Activate rest server")
+    lgr.Printf("[INFO] Host: %s", s.Host)
+    lgr.Printf("[INFO] Port: %s", s.Port)
+
+	return router
+}
+
+func (s Server) homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Home Page")
+}
